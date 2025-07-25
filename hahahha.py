4704f95d8f57fc49a6c15c2562c84c1561b32e80
Azrafin <azizrahmadarifin07@gmail.com>
2025-07-25 20:59:24 +0700

    nambahkan perintah print alamt

--- hahahha.py
+++ hahahha.py
@@ -5,4 +5,5 @@ hasil = a + b
 print(f"hasil penjumlahan a+b adalah = {hasil}")
 hitung= (f"angka 2 ditambah angka 4 hasilnya = {2 + 4}")
 ALAMAT= str("Jalan Proklamasi No.45 Jakarta Timur Indonesia")
+print(ALAMAT)
 
